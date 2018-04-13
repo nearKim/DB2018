7560c67accf5e154af371c6905b4508b72c5d5a7
nearKim <garfield@snu.ac.kr>
2018-04-13 15:23:54 +0900

    Add student Report feature

--- src/com/dev/StudentMenu.java
+++ src/com/dev/StudentMenu.java
@@ -131,11 +131,90 @@ public class StudentMenu {
     }
 
     private static void studentReport(Connection connection, String id) {
-        String reportIntro = "Welcome %s . \n You are a member of %s \n You have taken total %d credits. \n\n Semester Report \n\n";
-        String sql = "select name from student where ID = ? ";
+        String reportIntro = "Welcome %s . \nYou are a member of %s \nYou have taken total %d credits.\n\n Semester Report \n\n";
+        String introSql = "SELECT name, dept_name, tot_cred FROM student WHERE id = ?";
+
+        String sql = "select year, semester\n" +
+                "from takes ,student\n" +
+                "where takes.id=student.id  and takes.id=?\n" +
+                "group by year, semester\n" +
+                "order by year desc , CASE semester\n" +
+                "                                 WHEN 'Spring' THEN 1\n" +
+                "                                   WHEN 'Summer' THEN 2\n" +
+                "                                   WHEN 'Fall' THEN 3\n" +
+                "                                   WHEN 'Winter' THEN 4\n" +
+                "                              END desc";
+
+
+
+        try {
+            PreparedStatement yearSemPstmt = connection.prepareStatement(sql);
+            PreparedStatement introPstmt = connection.prepareStatement(introSql);
+            yearSemPstmt.setString(1, id);
+            introPstmt.setString(1, id);
+            ResultSet rsYearSem = yearSemPstmt.executeQuery();
+            ResultSet introResult = introPstmt.executeQuery();
+//
+            introResult.next();
+            System.out.println(String.format(reportIntro, introResult.getString(1),
+                                                            introResult.getString(2),
+                                                            introResult.getInt(3)));
+
+            while(rsYearSem.next()){
+                printReport(connection, id, rsYearSem.getInt("year"), rsYearSem.getString("semester"));
+//                System.out.println(rsYearSem.getString("year"));
+//                System.out.println(rsYearSem.getString("semester"));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void  printReport(Connection connection, String id, int year, String semester){
+
+        String sql = "WITH taken AS (SELECT name, student.dept_name AS s_dept, tot_cred, year, semester, course_ID, title, course.dept_name AS c_dept, credits, grade\n" +
+                "               FROM student NATURAL JOIN takes JOIN course USING(course_ID)\n" +
+                "               WHERE ID=?)\n" +
+                "SELECT name, s_dept, tot_cred, year, semester, gpa, course_ID, title, c_dept, credits, grade\n" +
+                "FROM taken NATURAL JOIN\n" +
+                "(SELECT year, semester, round(sum(credits* CASE grade\n" +
+                "                                        WHEN 'A+' THEN 4.3 WHEN 'A' THEN 4 WHEN 'A-' THEN 3.7\n" +
+                "                                        WHEN 'B+' THEN 3.3 WHEN 'B' THEN 3 WHEN 'B-' THEN 2.7\n" +
+                "                                        WHEN 'C+' THEN 2.3 WHEN 'C' THEN 2 WHEN 'C-' THEN 1.7\n" +
+                "                                        WHEN 'D+' THEN 1.3 WHEN 'D' THEN 1 WHEN 'D-' THEN 0.7\n" +
+                "                                        WHEN 'F' THEN 0 END)/sum(credits),5) AS GPA\n" +
+                "FROM taken\n" +
+                "WHERE taken.year= ? AND taken.semester = ? " +
+                "GROUP BY year, semester)\n" +
+                "ORDER BY year desc, CASE semester\n" +
+                "                    WHEN 'Spring' THEN 1\n" +
+                "                    WHEN 'Summer' THEN 2\n" +
+                "                    WHEN 'Fall' THEN 3\n" +
+                "                    WHEN 'Winter' THEN 4\n" +
+                "               END desc, course_ID";
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, id);
+            pstmt.setInt(2, year);
+            pstmt.setString(3, semester);
+
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()){
+                System.out.println("====================================");
+                System.out.println(rs.getInt("year")+"\t"+rs.getString("semester")+"\t"+rs.getFloat("gpa"));
+                System.out.println("course_id\t"+"title\t\t"+"dept_name\t"+"credits\t"+"grade");
+                System.out.print(rs.getString("course_id")+"\t");
+                System.out.print(rs.getString("title")+"\t");
+                System.out.print(rs.getString("c_dept")+"\t");
+                System.out.print(rs.getInt("credits")+"\t");
+                System.out.println(rs.getString("grade"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
-
 
 }
